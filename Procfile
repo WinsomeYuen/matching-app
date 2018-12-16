7f7f19c411a346804c22a2d11ba266a8954e0c2b
winsome <winsomeyuen@hotmail.com>
2018-12-16 20:29:01 +0000

    Changes made to Proc

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn sampledeploy.wsgi —-log-file -
+web: gunicorn matchingapp.wsgi —-log-file -
--- groupcoursework/settings.py
+++ groupcoursework/settings.py
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'groupcoursework.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
